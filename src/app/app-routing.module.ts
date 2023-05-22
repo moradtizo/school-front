594c9f119a49f104bf11e246e16326785c4de176
mibanas <sanabi_mohamed@hotmail.fr>
2023-05-22 15:50:32 +0100

    generate new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,71 @@
+import { Page404Component } from './authentication/page404/page404.component';
+import { AuthLayoutComponent } from './layout/app-layout/auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './layout/app-layout/main-layout/main-layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthentificationComponent } from './modules/authentification/page/authentification.component';
-import { DashbordComponent } from './modules/dashbord/page/dashbord.component';
-
+import { AuthGuard } from './core/guard/auth.guard';
+import { Role } from './core/models/role';
 const routes: Routes = [
   {
-    path : '',
-    component : DashbordComponent
+    path: '',
+    component: MainLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', redirectTo: '/authentication/signin', pathMatch: 'full' },
+      {
+        path: 'admin',
+        canActivate: [AuthGuard],
+        data: {
+          role: Role.Admin,
+        },
+        loadChildren: () =>
+          import('./admin/admin.module').then((m) => m.AdminModule),
+      },
+      {
+        path: 'teacher',
+        canActivate: [AuthGuard],
+        data: {
+          role: Role.Teacher,
+        },
+        loadChildren: () =>
+          import('./teacher/teacher.module').then((m) => m.TeacherModule),
+      },
+      {
+        path: 'student',
+        canActivate: [AuthGuard],
+        data: {
+          role: Role.Student,
+        },
+        loadChildren: () =>
+          import('./student/student.module').then((m) => m.StudentModule),
+      },
+
+      // Extra components
+      {
+        path: 'extra-pages',
+        loadChildren: () =>
+          import('./extra-pages/extra-pages.module').then(
+            (m) => m.ExtraPagesModule
+          ),
+      },
+      {
+        path: 'multilevel',
+        loadChildren: () =>
+          import('./multilevel/multilevel.module').then(
+            (m) => m.MultilevelModule
+          ),
+      },
+    ],
   },
   {
-    path : 'auth',
-    loadChildren: () => import('../app/modules/authentification/page/authentification.module').then(m => m.AuthentificationModule)
-  }
+    path: 'authentication',
+    component: AuthLayoutComponent,
+    loadChildren: () =>
+      import('./authentication/authentication.module').then(
+        (m) => m.AuthenticationModule
+      ),
+  },
+  { path: '**', component: Page404Component },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes, {})],
